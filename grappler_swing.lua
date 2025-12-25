554beddb6aba28e02fff3265462317ca1d14225c
thebiraf <raffertychambers@gmail.com>
2025-12-25 18:38:45 +0000

    Add loadstring setup for grappler swing scripts

---
+++ grappler_swing.lua
@@ -0,0 +1,4 @@
+-- my loadstring set up
+loadstring(game:HttpGet("https://raw.githubusercontent.com/random-projects-by-breif/grappler-swing-scriptst/refs/heads/main/patch-kill%20on%20chat.luau"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/random-projects-by-breif/grappler-swing-scriptst/refs/heads/main/perfect%20settings%20-%20fix.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/random-projects-by-breif/grappler-swing-scriptst/refs/heads/main/perfect%20disguise.lua"))();
